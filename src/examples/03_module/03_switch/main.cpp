0f79d7d9871d4adc0cd8c3af046efeb9cea841ec
Justin <justin.pesz@g.austincc.edu>
2020-09-15 18:19:17 -0500

    Switch

--- src/examples/03_module/03_switch/main.cpp
+++ src/examples/03_module/03_switch/main.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cin;
+using std::cout;
 
 int main() 
 {
-	
+	int option, y=0, z=0;
+
+	cout<<"Enter a number: ";
+	cin>>option;
+
+	std::string result = menu(option);
+
+	cout<<"\n"<<result;
+
+	option > 4 ? y=10 : z=20;
+
+	cout<<"\n"<<y<<" "<<z;
 
 	return 0;
 }
