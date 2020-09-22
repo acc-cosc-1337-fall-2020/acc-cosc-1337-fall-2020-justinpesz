f4bbb59b7c6bf2772bfeb996bb72e67e247dc0e2
Justin <justin.pesz@g.austincc.edu>
2020-09-22 17:23:46 -0500

    Homework 2

--- src/classwork/03_assign/decision.cpp
+++ src/classwork/03_assign/decision.cpp
@@ -1,1 +1,34 @@
 //cpp
+#include "decision.h"
+#include<string>
+
+std::string get_letter_grade_using_if(int grade)
+{
+    if(grade >= 90 && grade <= 100)
+    {
+        return "A";
+    }
+    if(grade >= 80 && grade < 90)
+    {
+        return "B";
+    }
+    if(grade >= 70 && grade < 80)
+    {
+        return "C";
+    }
+    if(grade >= 60 && grade < 70)
+    {
+        return "D";
+    }
+    if(grade >= 0 && grade < 60)
+    {
+        return "F";
+    }
+
+    return "";
+}
+
+/*std::string get_letter_grade_using_switch(int grade)
+{
+    
+}*/
--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,28 @@
 //Write the include statement for decisions.h here
+#include "decision.h"
+#include<iostream>
 
 //Write namespace using statements for cout and cin
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	char yesNo;
+	int grade;
 
+	do
+	{
+		cout<<"Enter numeric grade: ";
+		cin>>grade;
+
+		cout<<get_letter_grade_using_if(grade)<<"\n";
+
+		cout<<"Enter another? ";
+		cin>>yesNo;
+
+	} while (yesNo == 'y' || yesNo == 'Y');
+	
 	return 0;
 }
 
