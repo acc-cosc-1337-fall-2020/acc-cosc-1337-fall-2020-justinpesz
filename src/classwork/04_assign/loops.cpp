ef1f4d73315941f64554b61d13095846795b4e50
Justin <justin.pesz@g.austincc.edu>
2020-09-30 13:45:50 -0500

    Homework 3

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,14 @@
 //cpp
+#include "loops.h"
+
+int factorial(int num)
+{
+    int sum = 1;
+    
+    for (int i = 1; i <= num; ++i)
+    {
+        sum *= i;
+    }
+
+    return sum;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,10 @@
 //write includes statements
+#include "loops.h"
+#include<iostream>
 
 //write using statements for cin and cout
-
+using std::cin;
+using std::cout;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +13,30 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	char yesNo = 'y';
+	int num;
 
+	do
+	{
+		
+		cout<<"Enter a number from 1 to 10: ";
+		cin>>num;
+
+		if(num < 1 || num > 10)
+		{
+			cout<<"Invalid Entry!";
+		}
+
+		while(num >= 1 && num <= 10)
+		{
+			cout<<"\nThe factorial of "<<num<<" is "<<factorial(num);
+			num = 0;
+		}
+
+		cout<<"\nEnter another number? ";
+		cin>>yesNo;
+
+	} while (yesNo == 'y' || yesNo == 'Y');
+	
 	return 0;
 }
