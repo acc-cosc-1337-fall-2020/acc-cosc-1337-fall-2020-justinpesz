ddcc6117f8c96815082f0c324ff1dc1954351deb
Justin <justin.pesz@g.austincc.edu>
2020-10-20 17:07:32 -0500

    Homework 5

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,31 @@
+#include "tic_tac_toe.h"
+#include<iostream>
+#include<string>
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	TicTacToe tic_tac_toe;
+	std::string player;
 
-	return 0;
+	cout<<"Enter X or O: ";
+	cin>>player;
+
+	tic_tac_toe.start_game(player);
+
+	do
+	{
+		int position;
+		cout<<"Enter position 1 to 9: ";
+		cin>>position;
+		tic_tac_toe.mark_board(position);
+		tic_tac_toe.display_board();
+
+	}while(tic_tac_toe.game_over() == false);
+
+	cout<<"Game Over";
+
+	return 0;	
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,27 @@
 //h
+#include<vector>
+#include<string>
+#include<iostream>
+
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(std::string first_player);
+    void mark_board(int position);
+    std::string get_player()const;
+    void display_board()const;
+
+private:
+    bool check_board_full()const;
+    void set_next_player();
+    std::string player;
+    std::vector<std::string> pegs{9, " "};
+    void clear_board();
+};
+
+#endif
+
